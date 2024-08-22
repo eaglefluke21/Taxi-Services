a71391f40c004d09938b2defff432ebf7012f17f
eaglefluke21 <yadavankit7fc4@gmail.com>
2024-08-22 19:05:49 +0530

    index file

--- server/api/index.php
+++ server/api/index.php
@@ -1,14 +1,16 @@
 <?php
-include_once '../config/database.php';
-echo "Backend is Running.";
+// include_once '../config/database.php';
+// echo "Backend is Running.";
 
-$databaseObj = new Database();
-$db = $databaseObj->getConnection();
+// $databaseObj = new Database();
+// $db = $databaseObj->getConnection();
 
-if ($db) {
-    echo" Connected to database Successfully";
-} else{
-    echo " Failed to connect to database.";
-}
+// if ($db) {
+//     echo" Connected to database Successfully";
+// } else{
+//     echo " Failed to connect to database.";
+// }
+
+phpinfo();
 
 ?>
